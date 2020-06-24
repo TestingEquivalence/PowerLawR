cd17942240a76a1b6a61f29e567bc4560742e13d
Ostrovski <vladimir_ostrovski@web.de>
2020-06-24 10:04:50 +0200

    work with data

--- research city size.R
+++ research city size.R
@@ -52,9 +52,9 @@ for (beta in c(2.1,2.2,2.3,2.4,2.5)) {
 
 # compute test power at boundary points
 ###########################################
-parameter=list(kmin=20e3,kmax=10e6,scale=1e3,nSamples=1000,n=662, beta=2.3,
-               alpha=0.05, tol=0.001,nSimulation=1000, nDirections = 100,
-               test="MLE",eps=0.12,epsAdj=1)  
+parameter=list(kmin=20e3,kmax=10e6,scale=10e3,nSamples=1000,n=662, beta=2.15,
+               alpha=0.05, tol=0.001,nSimulation=1000,
+               test="bootstrap1",epsAdj=1)  
 
 
 for (eps in c(0.08,0.10,0.12)){
